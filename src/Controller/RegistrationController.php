d77e87a355e52038aaf8ea265997336f4075b93d
Corsicaa <jonathanbernardini92@gmail.com>
2022-04-28 17:40:49 +0200

    last version

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -14,7 +14,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/welcome", name="app_register")
+     * @Route("/register", name="app_register")
     */
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
