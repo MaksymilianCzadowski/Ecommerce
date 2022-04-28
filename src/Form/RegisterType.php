bfefd46e75110e948439bd7e792742a7bdf7d038
Corsicaa <jonathanbernardini92@gmail.com>
2022-04-28 17:39:54 +0200

    last version

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,15 +6,28 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('username')
-            ->add('password')
+            ->add('email', TextType::class, [
+            'attr' => [
+                'class' => 'form-control',
+                ]
+            ])
+            ->add('username', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    ]
+                ])
+            ->add('password', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    ]
+                ])
         ;
     }
 
