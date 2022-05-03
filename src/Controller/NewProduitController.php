4e9c9331176bf45610835107bfda2db7d4b9e534
czadowski <maksymilian.czadowski@ynov.com>
2022-05-03 17:27:56 +0200

    gre

--- src/Controller/NewProduitController.php
+++ src/Controller/NewProduitController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
 use App\Form\ProduitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,10 +14,21 @@ class NewProduitController extends AbstractController
     /**
      * @Route("/new/produit", name="app_new_produit")
      */
-    public function form(): Response
+    public function form(Request $request): Response
     {
+        $produit = new Produit;
         $form = $this->createForm(ProduitType::class);
         // createForm() permet de récupérer le formulaire :)
+        dump($request);
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($produit);
+            $manager->flush();
+            return $this->redirectToRoute('app_shop');
+        }
         return $this->render('new_produit/index.html.twig', [
             'formArticle' => $form->createView(),
         ]);
