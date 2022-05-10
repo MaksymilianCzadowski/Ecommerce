28ea5ed759e97fd68829d83cf6d01765294234ce
czadowski <maksymilian.czadowski@ynov.com>
2022-05-10 14:42:56 +0200

    aeze

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\Produit;
+use App\Entity\Commentaire;
 use App\Repository\ProduitRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Type;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ShopController extends AbstractController
@@ -31,4 +35,32 @@ class ShopController extends AbstractController
         ]);
     }
 
+     /**
+     * @Route("/shop/detail/{id}", name="app_detail")
+     */
+    public function commentaire(Request $request, Commentaire $commentaire): Response{
+        
+        $form = $this->createForm(commentaireType::class);
+        $form->handleRequest($request);
+
+        
+
+        if($form->isSubmitted() && $form->isValid())    // si on a fait une recherche
+        {
+            $commentaire = $form->get('contenu')->getData();
+            $commentaire->setCreatedAt(new DateTime());
+            $commentaire->setUser($this->getUser());
+            $commentaire->setProduit($this->getProduit());
+        }else{
+            return $this->render('shop/details.html.twig');
+        }
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+        
+        
+        return $this->render('shop/details.html.twig',[
+    
+        ]);
+    }
 }
