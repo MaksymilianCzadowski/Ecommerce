fd0c4e58c8e2269d73cd2639193508cb4522c951
Corsicaa <jonathanbernardini92@gmail.com>
2022-05-11 11:18:35 +0200

    oui

--- migrations/Version20220511090754.php
+++ migrations/Version20220511090754.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220511082206 extends AbstractMigration
+final class Version20220511090754 extends AbstractMigration
 {
     public function getDescription(): string
     {
